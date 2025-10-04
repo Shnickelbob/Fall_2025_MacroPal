c2b78b414f46a0378cea4368cedb8a998bfa5382
schaef46 <schaef46@email.franklin.edu>
2025-10-04 01:52:42 -0400

    Models dev branch
    This branch contains all five models outlined in the ERD and Requirements Document.
    Each model includes all attributes shown in the ERD, with some basic limits added for logical purposes.
    Since the ERD/Requirements specify integer values, any field using type: Number includes validation to ensure the input is a positive integer. If the input isn’t an integer, a message will appear telling the user their input must be an integer.
    I tried to keep comments simple and readable
    I hope the models meet your expectations

---
+++ MacroPal/backend/models/log.js
@@ -0,0 +1,42 @@
+/** 
+ * this model creates a log with five attributes (foods ids, exercises ids, recipes ids, date, and user id)
+ * 
+ * @author Brian Schaeffer
+ * @version October 4, 2025
+*/
+
+import mongoose from "mongoose"; // uses mongoose to build the schema and model
+
+const logSchema = new mongoose.Schema({ // creating the log schema
+
+    Foods_Ids: [{ // stores all the foods in the log
+        type: mongoose.Schema.Types.ObjectId, // ObjectId points to Food docs
+        ref: "Food", // linker
+    }],
+
+    Exercises_Ids: [{ // stores all the exercises in the log
+        type: mongoose.Schema.Types.ObjectId, // ObjectId points to Exercise docs
+        ref: "Exercise", // linker
+    }],
+
+    Recipes_Ids: [{ // stores all the recipes in the log
+        type: mongoose.Schema.Types.ObjectId, // ObjectId points to Recipe docs
+        ref: "Recipe", // linker
+    }],
+
+    Date: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+
+    User_Id: {
+        type: mongoose.Schema.Types.ObjectId, // ObjectId points back to the User who owns the log
+        ref: "User", // linker
+        required: true, // logs always belong to a user
+    },
+
+});
+
+const Log = mongoose.model("Log", logSchema); // makes a Log model
+export default Log; // allows other files to use Log models
