102d043847f011d28431e434b5ada1ac80bead3a
Donald.Chambers <chambd06@email.franklin.edu>
2025-10-19 01:35:52 -1000

    Added progress bar and nav buttons

--- MacroPal/frontend/src/pages/homepage.jsx
+++ MacroPal/frontend/src/pages/homepage.jsx
@@ -13,12 +13,16 @@ import { useState } from 'react';
 import "../App.css";
 import ModalAddFood from "../Components/ModalAddFood";
 import Menu from "../Components/Menu";
+import { FaPlus, FaList} from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { BsPencilFill } from "react-icons/bs";
+import ProgressBar from "../Components/ProgressBar";
 
 function HomePage() {
   const [open, setOpen] = useState(false); // for menu
 
   return (
-    <div className="" style={{ padding: 24 }}>
+    <div className="" style={{ }}>
 
     {/* // Menu component: */}
     <Menu 
@@ -26,16 +30,72 @@ function HomePage() {
     setOpen={setOpen}
     />
 
-    <h2>Test Homepage</h2>
-    <p>Adding menu and add food buttons for now</p>
+    <h2>USER'S Daily Goals</h2>
+    <p>Progress bars for visual indications</p>
+    <ProgressBar
+      label="Calories"
+      bgcolor="#D62E4E"
+      height={25}
+      part={850}
+      total={2000}
+    />
+
+    <ProgressBar
+      label="Protein"
+      bgcolor="#81ACA6"
+      height={25}
+      part={55}
+      total={100}
+    />
+
+    <ProgressBar
+      label="Carbs"
+      bgcolor="#E8A202"
+      height={25}
+      part={10}
+      total={75}
+    />
+
+    <ProgressBar
+      label="Fat"
+      bgcolor="#B0D095"
+      height={25}
+      part={15}
+      total={40}
+    />
 
     {/* // Add Food Button */}
     <button
-        className="mp-btn mp-btn-primary"
+        title="Edit goals"
+        className="mp-btn-homepage"
         style={{ position: "absolute", top: 20, right: 20 }}
         onClick={() => window.location.href = "/demo"}
         >
-        Add Food Demo
+        <BsPencilFill />
+    </button>
+    <button
+        title="Add food to the database"
+        className="mp-btn-homepage"
+        style={{ position: "absolute", bottom: 20, right: 20 }}
+        onClick={() => window.location.href = "/demo"}
+        >
+        <FaPlus />
+    </button>
+    <button
+        title="Search for food items"
+        className="mp-btn-homepage"
+        style={{ position: "absolute", bottom: 20, right: 70 }}
+        onClick={() => window.location.href = "/search"}
+        >
+        <FaMagnifyingGlass />
+    </button>
+    <button
+        title="View daily log"
+        className="mp-btn-homepage"
+        style={{ position: "absolute", bottom: 20, left: 20 }}
+        onClick={() => window.location.href = "/demo"}
+        >
+        <FaList />
     </button>
 
     </div>
